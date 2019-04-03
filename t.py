73b82c56b306efeac19ebfd22f89be425ab5be4e
ShourjaMukherjee <shourja123@gmail.com>
2019-04-04 02:15:16 +0530

    Added values to overflow,binary finalans and little bit of readability

--- t.py
+++ t.py
@@ -1,3 +1,9 @@
+def bintodec(n):
+    return int(n,2)
+
+def dectobin(n):
+    return "{0:b}".format(n)
+
 def two_comp(lst):
 	val=0
 	val -= lst[3]*8
@@ -7,7 +13,7 @@ def two_comp(lst):
 
 def one_comp(lst):
 	val=0
-	val -= lst[3]*8 
+	val -= lst[3]*8
 
 	if lst[3] == 1:
 		val += 1
@@ -23,18 +29,17 @@ def signed(lst):
 		val = -val
 	return val
 
+#MAIN
 a,b=input().split()
 
-c = int(a,2) + int(b,2)
-a=int(a)
-b=int(b)
-val_a = a
-val_b = b
+binary_sum = bintodec(a) + bintodec(b)
+val_a = int(a)
+val_b = int(b)
 i=0
 arr_a=[]
 arr_b=[]
 
-while i < 4:
+while i < 4: #loop to convert a and b into binary arrays
 	arr_a.insert(i,val_a % 10)
 	arr_b.insert(i,val_b % 10)
 	val_a //= 10
@@ -45,11 +50,11 @@ result=[]
 carry=0
 i=0
 
-for i in range(4):
+for i in range(4): #loop to do binary addition
  	result.insert(i,arr_a[i]^arr_b[i]^carry)
  	carry = arr_a[i]&arr_b[i]
 
-result_binary=""
+result_binary=dectobin(binary_sum)
 
 # for i in result:
 # 	s = str(i)
@@ -59,36 +64,37 @@ result_binary=""
 # print(result)
 # result_binary = result_binary.join(result)
 
-result_unsigned = c
+result_unsigned = binary_sum
 
-if carry == 1:
-	result_binary = "overflow"
-if c > 15:
-	result_unsigned = "overflow"
+if binary_sum > 15:
+	result_unsigned = "overflow (" + str(binary_sum) + ")"
 
 a_signed = signed(arr_a)
 b_signed = signed(arr_b)
 result_signed = a_signed + b_signed
+answer_signed = result_signed
 
 if result_signed < -7 or result_signed > 7:
-	result_signed = "overflow"
+	answer_signed = "overflow (" + str(result_signed) + ")"
 
 a_two_comp=two_comp(arr_a)
 b_two_comp=two_comp(arr_b)
 result_two_comp=a_two_comp+b_two_comp
+answer_two_comp=result_two_comp
 
 a_one_comp=one_comp(arr_a)
 b_one_comp=one_comp(arr_b)
 result_one_comp=a_one_comp+b_one_comp
+answer_one_comp= result_one_comp
 
 if(result_two_comp < -8 or result_two_comp > 7 ):
-	result_two_comp="overflow"
+	answer_two_comp="overflow (" + str(result_two_comp) + ")"
 
 if(result_one_comp < -7 or result_one_comp > 7 ):
-	result_one_comp="overflow"
+	answer_one_comp="overflow (" + str(result_one_comp) + ")"
 
-#print("binary value:" ,result_binary)
+print("binary representation:" ,result_binary)
 print("unsigned value:" ,result_unsigned)
-print("signed value:" ,result_signed)
-print("two complement value:" ,result_two_comp)
-print("one complement value:" ,result_one_comp)
+print("signed value:" ,answer_signed)
+print("two complement value:" ,answer_two_comp)
+print("one complement value:" ,answer_one_comp)
